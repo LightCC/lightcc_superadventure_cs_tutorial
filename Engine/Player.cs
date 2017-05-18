2de26b92fb9cfe448d4a2aafd08de0be7061291c
LightCC <light_cc@yahoo.com>
2017-05-17 23:22:48 -0400

    Add project files.

---
+++ Engine/Player.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Player : LivingCreature
+    {
+        public int Gold { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
+        public List<InventoryItem> Inventory { get; set; }
+        public List<PlayerQuest> Quests { get; set; }
+        public Location CurrentLocation { get; set; }
+        
+        public Player(int gold, int experiencePoints, int level,
+            int currentHitPoints, int maximumHitPoints)
+            : base(currentHitPoints, maximumHitPoints)
+        {
+            Gold = gold;
+            ExperiencePoints = experiencePoints;
+            Level = level;
+            Inventory = new List<InventoryItem>();
+            Quests = new List<PlayerQuest>();
+        }
+
+        public bool HasRequiredItemToEnterThisLocation(Location location)
+        {
+            if(location.ItemRequiredToEnter == null)
+            {
+                // There is no required item for this location, so return "true"
+                return true;
+            }
+
+            // See if the player has the required item in their inventory
+            foreach (InventoryItem ii in Inventory)
+            {
+                if(ii.Details.ID == location.ItemRequiredToEnter.ID)
+                {
+                    // We found the required item, so return "true"
+                    return true;
+                }
+            }
+
+            // We didn't find the required item in their inventory, so return "false"
+            return false;
+        }
+
+        public bool HasThisQuest(Quest questToCheck)
+        {
+            foreach (PlayerQuest playerQuest in Quests)
+            {
+                if(questToCheck.ID == playerQuest.Details.ID)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool CompletedThisQuest(Quest questToCheck)
+        {
+            foreach(PlayerQuest playerQuest in Quests)
+            {
+                if(playerQuest.Details.ID == questToCheck.ID)
+                {
+                    return playerQuest.IsCompleted;
+                }
+            }
+
+            return false;
+        }
+
+        public int ItemQuantity(Item item)
+        {
+            foreach (InventoryItem ii in Inventory)
+            {
+                if(ii.Details.ID == item.ID)
+                {
+                    return ii.Quantity;
+                }
+            }
+
+            // Did not find the item in this player's inventory
+            return 0;
+        }
+
+        public bool HasAllQuestCompletionItems(Quest quest)
+        {
+            // See if the player has all the items needed
+            // to complete the quest here
+            foreach (QuestCompletionItem qci in quest.QuestCompletionItems)
+            {
+                bool foundItemInPlayersInventory = false;
+
+                // Check each item in the player's inventory,
+                // to see if they have it, and enough of it
+                foreach(InventoryItem ii in Inventory)
+                {
+                    // The player has the item in their inventory
+                    if(ii.Details.ID == qci.Details.ID)
+                    {
+                        foundItemInPlayersInventory = true;
+                        // The player does not have enough of this item
+                        // to complete the quest
+                        if(ii.Quantity < qci.Quantity)
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+                // The player does not have any of this quest
+                // completion item in their inventory
+                if (!foundItemInPlayersInventory)
+                {
+                    return false;
+                }
+            }
+
+            // If we got here, then the player must have all the required
+            // items, and enough of them, to complete the quest.
+            return true;
+        }
+
+        public void RemoveQuestCompletionItems(Quest quest)
+        {
+            foreach (QuestCompletionItem qci in quest.QuestCompletionItems)
+            {
+                foreach (InventoryItem ii in Inventory)
+                {
+                    if(ii.Details.ID == qci.Details.ID)
+                    {
+                        // Subtract quantity from player's inv
+                        // that is needed to complete quest
+                        ii.Quantity -= qci.Quantity;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void AddItemToInventory(Item itemToAdd)
+        {
+            foreach (InventoryItem ii in Inventory)
+            {
+                if(ii.Details.ID == itemToAdd.ID)
+                {
+                    // They have the item in their inventory, so
+                    // increase the quantity by one
+                    ii.Quantity++;
+
+                    return; // We added the item, and are done
+                }
+            }
+
+            // They didn't have the item, so add it to their inventory
+            Inventory.Add(new InventoryItem(itemToAdd, 1));
+        }
+
+        // NOTE:  If the player does not have 'quest', this will do nothing
+        public void MarkQuestCompleted(Quest quest)
+        {
+            // Find the quest in the player's quest list
+            foreach (PlayerQuest pq in Quests)
+            {
+                if (pq.Details.ID == quest.ID)
+                {
+                    // Mark it as completed
+                    pq.IsCompleted = true;
+
+                    // We found the quest, and marked it complete, so get
+                    // out of this function
+                    return;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Engine/RandomNumberGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Engine
+{
+    public static class RandomNumberGenerator
+    {
+        /* 
+        // Simple random numbers - not as random as you'd like, though
+        private static Random _generator = new Random();
+
+        public static int NumberBetween(int minimumValue, int maximumValue)
+        {
+            return _generator.Next(minimumValue, maximumValue + 1)
+        }
+        */
+
+        // This version is better at not following a pattern when generating
+        // random numbers
+        private static readonly RNGCryptoServiceProvider _generator =
+            new RNGCryptoServiceProvider();
+
+        public static int NumberBetween(int minValue, int maxValue)
+        {
+            byte[] randomNumber = new byte[1];
+
+            _generator.GetBytes(randomNumber);
+
+            double asciiValueOfRandomCharacter = Convert.ToDouble(randomNumber[0]);
+
+            // We are using Math.Max, and substracting 0.00000000001,
+            // to ensure "multiplier" will always be between 0.0 and .99999999999
+            // Otherwise, it's possible for it to be "1", which causes problems in our rounding.
+            double multiplier = Math.Max(0, (asciiValueOfRandomCharacter / 255d) - 0.00000000001d);
+            
+            // We need to add one to the range, to allow for the rounding done with Math.Floor
+            int range = maxValue - minValue + 1;
+
+            double randomValueInRange = Math.Floor(multiplier * range);
+
+            return (int)(minValue + randomValueInRange);
+        }
+    }
+}
