5ff9c50407c2fa915040a6044020684ad6f5ca42
LightCC <light_cc@yahoo.com>
2017-05-29 01:33:05 -0400

    21.2 Added Vendor Class * Added Vendor.cs class (Name and Inventory properties), methods AddItemToInventory and RemoveItemFromInventory and events that indicate when vendor Inventory changes for the UI to use. * Added Location.VendorWorkingHere Property to tell us when a location has a vendor (only 1 allowed per location) * Added a test vendor to the town square

---
+++ Engine/Vendor.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Engine
+{
+    public class Vendor : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public BindingList<InventoryItem> Inventory { get; private set; }
+
+        public Vendor(string name)
+        {
+            Name = name;
+            Inventory = new BindingList<InventoryItem>(); // Start null - no items
+        }
+
+        public void AddItemToInventory(Item itemToAdd, int quantity = 1)
+        {
+            InventoryItem item = Inventory.SingleOrDefault(
+                ii => ii.Details.ID == itemToAdd.ID);
+
+            if(item == null)
+            {
+                // They didn't have the item, so add it to their inventory
+                Inventory.Add(new InventoryItem(itemToAdd, quantity));
+            }
+            else
+            {
+                // They already have the item, so just increase the quantity
+                item.Quantity += quantity;
+            }
+
+            OnPropertyChanged("Inventory");
+        }
+
+        public void RemoveItemFromInventory(Item itemToRemove, int quantity = 1)
+        {
+            InventoryItem item = Inventory.SingleOrDefault(
+                ii => ii.Details.ID == itemToRemove.ID);
+
+            if(item == null)
+            {
+                // The item is not in player's inventory, so ignore it
+                // We might want to raise an error here...
+            }
+            else
+            {
+                // They have the item in their inventory, so decrease the quantity
+                item.Quantity -= quantity;
+
+                // Don't allow negative quantities.
+                // We might want to raise an error here...
+                if(item.Quantity < 0)
+                {
+                    item.Quantity = 0;
+                }
+
+                // If the quantity is zero, remove the item from the list
+                if(item.Quantity == 0)
+                {
+                    Inventory.Remove(item);
+                }
+
+                // Notify the UI that the inventory has changed
+                OnPropertyChanged("Inventory");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string name)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
