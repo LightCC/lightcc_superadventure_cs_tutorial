252be0ba1d0ed957d8bb571cb57639c881702e66
LightCC <light_cc@yahoo.com>
2017-05-30 02:13:23 -0400

    21.4e Add buy/sell logic to TradingScreen * Add logic to Trading Screen so that clicks on the "Buy 1" and "Sell 1" button columns in the Player and Vendor inventory dgv's will complete the purchase or sale.

--- SuperAdventure/TradingScreen.cs
+++ SuperAdventure/TradingScreen.cs
@@ -91,13 +91,13 @@ namespace SuperAdventure
                 DataPropertyName = "Description"
             });
 
-            dgvVendorItems.Columns.Add(new DataGridViewTextBoxColumn
-            {
-                HeaderText = "Qty",
-                Width = 30,
-                DefaultCellStyle = rightAlignedCellStyle,
-                DataPropertyName = "Quantity"
-            });
+            //dgvVendorItems.Columns.Add(new DataGridViewTextBoxColumn
+            //{
+            //    HeaderText = "Qty",
+            //    Width = 30,
+            //    DefaultCellStyle = rightAlignedCellStyle,
+            //    DataPropertyName = "Quantity"
+            //});
 
             dgvVendorItems.Columns.Add(new DataGridViewTextBoxColumn
             {
@@ -124,12 +124,61 @@ namespace SuperAdventure
 
         private void dgvMyItems_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            throw new NotImplementedException();
+            // The first column of a datagridview has a ColumnIndex = 0
+            // This is known as a "zero-based" array/collection/list
+            // You start counting with 0.
+            //
+            // The 5th column (ColumnIndex = 4) is the column with the button.
+            // So, if the player clicked the button column, we will sell an item from that row.
+            if(e.ColumnIndex == 4)
+            {
+                // This gets the ID value of the item, from the hidden 1st column
+                // Remember, ColumnIndex = 0, for the first column
+                var itemID = dgvMyItems.Rows[e.RowIndex].Cells[0].Value;
+
+                // Get the Item object for the selected item row
+                Item itemBeingSold = World.ItemByID(Convert.ToInt32(itemID));
+
+                if(itemBeingSold.Price == World.UNSELLABLE_ITEM_PRICE)
+                {
+                    MessageBox.Show("You cannot sell the " + itemBeingSold.Name);
+                }
+                else
+                {
+                    // Remove one of these items from the player's inventory
+                    _currentPlayer.RemoveItemFromInventory(itemBeingSold);
+
+                    // Give the player the gold for the item being sold.
+                    _currentPlayer.Gold += itemBeingSold.Price;
+                }
+            }
         }
 
         private void dgvVendorItems_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            throw new NotImplementedException();
+            // The 4th column (ColumnIndex = 3) has the "Buy 1" button.
+            if (e.ColumnIndex == 3)
+            {
+                // This gets the ID value of the item, from the hidden 1st column
+                var itemID = dgvVendorItems.Rows[e.RowIndex].Cells[0].Value;
+
+                // Get the Item object for the selected item row
+                Item itemBeingBought = World.ItemByID(Convert.ToInt32(itemID));
+
+                // Check if the player has enough gold to buy the item
+                if(_currentPlayer.Gold >= itemBeingBought.Price)
+                {
+                    //Add one of the items to the player's inventory
+                    _currentPlayer.AddItemToInventory(itemBeingBought);
+
+                    // Remove the gold to pay for the item
+                    _currentPlayer.Gold -= itemBeingBought.Price;
+                }
+                else
+                {
+                    MessageBox.Show("You do not have enough gold to buy the " + itemBeingBought.Name);
+                }
+            }
         }
 
         private void btnClose_Click(object sender, EventArgs e)
