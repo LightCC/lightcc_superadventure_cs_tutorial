d7acf279ad786e878b7e4dbf6155f7aa3b98641f
Mike Leiter <light_cc@yahoo.com>
2017-10-14 21:25:51 -0400

    Chap 26.1.5 Added Images to WorldMap
    * Added picturebox grids to WorldMap Form
    * Updated WorldMap Class with SetImage Method and initialized all images
    * Removed Trader.png and added Bridge.png

--- SuperAdventure/WorldMap.cs
+++ SuperAdventure/WorldMap.cs
@@ -1,20 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
+using System.IO;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace SuperAdventure
 {
     public partial class WorldMap : Form
     {
+        readonly Assembly _thisAssembly = Assembly.GetExecutingAssembly();
+
         public WorldMap()
         {
             InitializeComponent();
+
+            SetImage(pic_0_2, "HerbalistsGarden");
+            SetImage(pic_1_2, "HerbalistsHut");
+            SetImage(pic_2_0, "FarmFields");
+            SetImage(pic_2_1, "Farmhouse");
+            SetImage(pic_2_2, "TownSquare");
+            SetImage(pic_2_3, "TownGate");
+            SetImage(pic_2_4, "Bridge");
+            SetImage(pic_2_5, "SpiderForest");
+            SetImage(pic_3_2, "Home");
         }
+
+        private void SetImage(PictureBox pictureBoxTarget, string imageName)
+        {
+            using (Stream resourceStream = _thisAssembly.GetManifestResourceStream(_thisAssembly.GetName().Name + ".Images." + imageName + ".png"))
+            {
+                if (resourceStream != null)
+                {
+                    pictureBoxTarget.Image = new Bitmap(resourceStream);
+                }
+            }
+        }
+
     }
 }
